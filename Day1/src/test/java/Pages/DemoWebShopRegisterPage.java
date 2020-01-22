6116a4bb18f27f709c0e880bbd4142de58e91ccf
Gokulapriya <g.periannan@accenture.com>
2020-01-22 11:42:36 +0530

    POM

---
+++ Day1/src/test/java/Pages/DemoWebShopRegisterPage.java
@@ -0,0 +1,25 @@
+package Pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class DemoWebShopRegisterPage {
+	WebDriver driver;
+	
+	WebElement gender;
+	WebElement firstName;
+	WebElement lastName;
+	WebElement email;
+	WebElement password;
+	WebElement confirmPassword;
+	WebElement register;
+	public void clickRegister()
+	{
+		
+	}
+	public DemoWebShopRegisterPage(WebDriver driver) {
+		super();
+		this.driver = driver;
+	}
+	
+}
